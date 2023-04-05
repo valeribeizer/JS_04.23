0a6edada5b70362e13038829fa7529cd498855e3
Valeryia Beizer <strelcova.lera94@gmail.com>
2023-04-05 15:40:04 +0200

    [1.2:05.04]: second task finished

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+///////////////TASK_1//////////////////
 function generateAnswer() {
   const input = Number(prompt("Enter a valid number please!"));
 
@@ -11,26 +12,26 @@ function generateAnswer() {
   } else {
     console.log("Incorrect input!");
     generateAnswer();
-  };
-};
+  }
+}
 
 function factorial(num) {
   let result = 1;
-  
+
   while (num) {
     result *= num--;
-  };
+  }
   return result;
-};
+}
 
 function isPrime(num) {
   for (let i = 2; i <= num - 1; i++) {
     if (num % i === 0) {
       return false;
-    };
-  };
+    }
+  }
   return true;
-};
+}
 
 function delimiters(num) {
   const delimiters = [];
@@ -38,9 +39,45 @@ function delimiters(num) {
   for (let i = num; i > 0; i--) {
     if (num % i === 0) {
       delimiters.push(i);
-    };
-  };
+    }
+  }
   return delimiters.length > 0 ? delimiters.join() : "There are no delimiters!";
-};
+}
 
 generateAnswer();
+
+///////////////TASK_2//////////////////
+function generateSymbols() {
+  const inputLetters = prompt("Enter 3 symbols. Spaces are not allowed!");
+  const trimedInput = inputLetters.trim();
+  const whiteSpace = " ";
+  const whiteSpaceValidation = trimedInput.indexOf(whiteSpace);
+
+  if (whiteSpaceValidation === -1 && trimedInput.length > 0 && trimedInput.length < 4) {
+    generateNumber(trimedInput);
+  } else {
+    console.log("Incorrect input!");
+    generateSymbols();
+  }
+}
+
+function generateNumber(inp) {
+  const inputNumber = Number(prompt(`Now it's time to give us a valid number from 1 to 9!`));
+
+  if (inputNumber > 0 && inputNumber < 10 && inputNumber % 1 === 0) {
+    let pattern = "";
+
+    for (let i = 1; i <= inputNumber; i++) {
+      for (let j = 1; j <= inputNumber; j++) {
+        pattern += inp + " ";
+      }
+      pattern += "\n";
+    }
+    console.log(pattern);
+  } else {
+    console.log("Incorrect input!");
+    generateNumber(inp);
+  }
+}
+
+generateSymbols();
