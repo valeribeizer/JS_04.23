557efb94ac3e02f132b5b5f989052e823c31d515
Valeryia Beizer <strelcova.lera94@gmail.com>
2023-04-09 21:23:28 +0200

    [2.2:09.04]: second task finished

--- index.js
+++ index.js
@@ -15,3 +15,25 @@ function makeDeepCopy(obj) {
   }
   return clonedObj;
 }
+
+//////////////TASK_2///////////////////
+function selectFromInterval(numArr, num1, num2) {
+  let arrValidation;
+  const numValidation = (num) => typeof num === "number" && isFinite(num);
+
+  if (Array.isArray(numArr) && numArr.length !== 0) {
+    arrValidation = numArr.every((el) => typeof el === "number" && isFinite(el));
+  } else {
+    throw new Error();
+  }
+
+  if (!arrValidation || !numValidation(num1) || !numValidation(num2)) {
+    throw new Error();
+  }
+
+  const start = Math.min(num1, num2);
+  const end = Math.max(num1, num2);
+  const newArr = numArr.filter((el) => el >= start && el <= end);
+
+  return newArr;
+}
